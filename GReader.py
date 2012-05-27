d2674e7a70776264889ff622528344f3bdb116c5
Terry Drymonacos <tdrymonacos@aligntech.com>
2012-05-27 08:40:53 -0700

    Listing reader elements continued:- -Tags listing -Article listing -added and reorganized a few more tests

--- GReader.py
+++ GReader.py
@@ -6,18 +6,24 @@ import pycurl
 import cStringIO
 import re
 import json
+import time
 
 import browserHTTP
 
 
+#########################################################################
+class googleReaderError(Exception):    
+    """Processing Error Exception stub """
+ 
 
 
 ########################################################################
-class  grInterface(object):
+class  googleReader(object):
     """ Simple interface for Google Reader"""
     #----------------------------------------------------------------------
     def __init__(self):
         """ Constructor """
+        self.myName = 'FeedFiddler'
         self.apiURL = 'http://www.google.com/reader/api/0/'
         self.tokenURL = self.apiURL+'token'
         self.loginURL = 'https://www.google.com/accounts/ClientLogin'
@@ -36,6 +42,7 @@ class  grInterface(object):
         #session Token (Set to None before Login)
         self.SessionToken = None
         
+    #----------------------------------------------------------------------
     def login(self):
         """Login to the service with your built in authentication method"""
         #This is will also register the security auth value, this is the openid session.
@@ -67,34 +74,76 @@ class  grInterface(object):
                     #register and STORE the session token
                     self.SessionToken = isMatch.group(3)
                     break
+        #raise an error condition if we are not able to find the Auth value
+        if self.SessionToken == None:
+            raise googleReaderError('Invalid login: SessionToken was not generated')
                 
+    #----------------------------------------------------------------------
     def getToken(self):
         """Get the token """
         self.browser.txHeaders = {}
         self.browser.txHeaders['authorization']= 'GoogleLogin auth=%s' % self.SessionToken
         return self.browser.get(self.tokenURL)
         
+    #----------------------------------------------------------------------
+    def getUserInfo(self):
+        """Get the token """
+        self.browser.txHeaders = {}
+        self.browser.txHeaders['authorization']= 'GoogleLogin auth=%s' % self.SessionToken
+        self.tokenURL = self.apiURL+'user-info'
         
+        getParms = {}
+        getParms['ck'] = int(time.time())
+        getParms['application'] = self.myName
+        return json.loads(self.browser.get(self.tokenURL))['userId']
+        
+    #----------------------------------------------------------------------
     def listFeeds(self):
-        """List the feeds """
+        """List the feeds and return a list of subscriptions"""
         
+        result = None
         subListURL = self.apiURL+'subscription/list?output=json'
-        try:
-            self.browser.postData = None
-            self.browser.txHeaders['authorization']= 'GoogleLogin auth=%s' % self.SessionToken
-            result = self.browser.get(subListURL)
-            jsonResults = json.loads(result)
-            
-            #print all the titles that we have subscriptions to
-            for elem in jsonResults['subscriptions']:
-                print elem['title']
-                
-            
-        except browserHTTP.processingHTTPError, e:
-            
-            result = None
-        x = 'foo'
+        self.browser.postData = None
+        self.browser.txHeaders['authorization']= 'GoogleLogin auth=%s' % self.SessionToken
+        result = self.browser.get(subListURL)
+        jsonResults = json.loads(result)
+        result = jsonResults['subscriptions']
+        return result
+    #----------------------------------------------------------------------
+    def listTags(self):
+        """List the tags and return a list of tags"""
         
-    
+        result = None
+        subListURL = self.apiURL+'tag/list?output=json'
+        self.browser.postData = None
+        self.browser.txHeaders['authorization']= 'GoogleLogin auth=%s' % self.SessionToken
+        result = self.browser.get(subListURL)
+        jsonResults = json.loads(result)
+        result = jsonResults['tags']
+        return result
+
+    #----------------------------------------------------------------------
+    def listArticles(self):
+        """List all the articles"""
+        result = None
+        
+        
+        getParms = {}
+        getParms['output'] = 'json'
+        getParms['co'] = 'True'
+        getParms['r'] = 'n'
+        getParms['n'] = 500
+        getParms['ck'] = int(time.time())
+        getParms['client'] = self.myName
+        self.contentURL = self.apiURL+'stream/contents/user/%s/state/com.google/' % self.getUserInfo()
+        
+        subListURL = self.contentURL+'reading-list?%s' % urllib.urlencode(getParms)
+        self.browser.txHeaders['authorization']= 'GoogleLogin auth=%s' % self.SessionToken
+        result = self.browser.get(subListURL)
+        jsonResults = json.loads(result)
+        result = jsonResults['items']
+        for elem in result:
+            print elem['title']
+        return result
     
     
--- testGReader.py
+++ testGReader.py
@@ -8,7 +8,7 @@ import GReader
 
 ########################################################################
 class TestGoogleReaderSimple (unittest.TestCase):
-	"""Test the Google Reader Interface"""
+	""" Elementary tests for logging into the Google Reader Authentication system"""
 	#----------------------------------------------------------------------
 	def setUp(self):
 		"""Initialization"""
@@ -16,14 +16,18 @@ class TestGoogleReaderSimple (unittest.TestCase):
 	#----------------------------------------------------------------------
 	def testGoogleReaderLogin(self):
 		""" Test that we can connect to our google reader account"""
-		logger.debug('attempting to  activate google reader account')
-		ggAccount  = GReader.grInterface()
-		
-		
-		myID = ggAccount.login()
+		try:
+			#create our reader
+			ggAccount  = GReader.googleReader()
+			
+			#login to google
+			ggAccount.login()
+		except GReader.googleReaderError,e:
+			self.fail()
+			
 		
 		#see if we can list our feeds
-		ggAccount.listFeeds()
+		#ggAccount.listFeeds()
 		
 		#result = myBrowser.post(targetURL)
 		#if re.search("Feeling Lucky", result):
@@ -31,11 +35,80 @@ class TestGoogleReaderSimple (unittest.TestCase):
 		#else:
 			#self.fail()
 	#----------------------------------------------------------------------
+	def testGoogleReaderBadLogin(self):
+		""" Attempt to login using bad credentials account"""
+		try:
+			ggAccount  = GReader.googleReader()
+		
+			#monkeypatch it so that the password is wrong
+			ggAccount.password = "wrongPassword"
+			ggAccount.login()
+			
+			#if we got this far, its bad, really bad
+			self.fail()
+		except GReader.googleReaderError:
+			pass
+	#----------------------------------------------------------------------
+	def tearDown(self):
+		pass
+	
+	
+########################################################################
+class TestGoogleReaderListings(unittest.TestCase):
+	"""Test the Google Reader Interface Listing capabilities"""
+	#----------------------------------------------------------------------
+	def setUp(self):
+		"""Initialization"""
+		#create our reader
+		ggAccount  = GReader.googleReader()
+		
+		#login to google
+		ggAccount.login()
+		self.ggAccount = ggAccount
+		
+	#----------------------------------------------------------------------
+	def testGoogleReaderSubscriptionList(self):
+		""" Test that we can connect to our google reader account"""
+		
+		try:
+			resList = self.ggAccount.listFeeds()
+			
+			if not resList:
+				self.fail()
+			
+		except GReader.googleReaderError,e:
+			self.fail()
+	#----------------------------------------------------------------------
+	def testGoogleReaderTagList(self):
+		""" Test that we can connect to our google reader account"""
+		
+		try:
+			resList = self.ggAccount.listTags()
+			
+			if not resList:
+				self.fail()
+				
+		except GReader.googleReaderError,e:
+			self.fail()
+	#----------------------------------------------------------------------
+	def testGoogleReaderArticleList(self):
+		""" Test that we can connect to our google reader account"""
+		
+		try:
+			resList = self.ggAccount.listArticles()
+			
+			if not resList:
+				self.fail()
+				
+		except GReader.googleReaderError,e:
+			self.fail()
+	#----------------------------------------------------------------------
 	def tearDown(self):
 		pass
 
 
 def getSuite():
 	suiteList = []
-	suiteList.append(unittest.makeSuite(TestGoogleReaderSimple, 'test'))
+	suiteList.append(unittest.makeSuite(TestGoogleReaderSimple))
+	suiteList.append(unittest.makeSuite(TestGoogleReaderListings))
 	return unittest.TestSuite(suiteList)
